7e5d0e8ec7e6b3e61c781438c1fb34a2f4fe344f
moraismariana <moraismariana200@gmail.com>
2025-05-15 19:19:58 -0300

    Mudança temporária de permissões Francoadv

--- francoadv/views.py
+++ francoadv/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from francoadv.models import PaginaInicio, PaginaSobre, PaginaServicos, ComponenteInsta, ComponenteContato, ComponenteCTA
 
@@ -7,23 +8,29 @@ from francoadv.serializers import PaginaInicioSerializer, PaginaSobreSerializer,
 class PaginaInicioViewSet(viewsets.ModelViewSet):
     queryset = PaginaInicio.objects.all()
     serializer_class = PaginaInicioSerializer
+    permission_classes = [AllowAny]
 
 class PaginaSobreViewSet(viewsets.ModelViewSet):
     queryset = PaginaSobre.objects.all()
     serializer_class = PaginaSobreSerializer
+    permission_classes = [AllowAny]
 
 class PaginaServicosViewSet(viewsets.ModelViewSet):
     queryset = PaginaServicos.objects.all()
     serializer_class = PaginaServicosSerializer
+    permission_classes = [AllowAny]
 
 class ComponenteInstaViewSet(viewsets.ModelViewSet):
     queryset = ComponenteInsta.objects.all()
     serializer_class = ComponenteInstaSerializer
+    permission_classes = [AllowAny]
 
 class ComponenteContatoViewSet(viewsets.ModelViewSet):
     queryset = ComponenteContato.objects.all()
     serializer_class = ComponenteContatoSerializer
+    permission_classes = [AllowAny]
 
 class ComponenteCTAViewSet(viewsets.ModelViewSet):
     queryset = ComponenteCTA.objects.all()
     serializer_class = ComponenteCTASerializer
+    permission_classes = [AllowAny]
