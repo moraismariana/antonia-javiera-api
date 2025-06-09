5618ca67dc4885acbdf564bde0bc67d2d209e4eb
moraismariana <moraismariana200@gmail.com>
2025-06-09 15:05:07 -0300

    CORS Atualização teste

--- setup/settings.py
+++ setup/settings.py
@@ -144,7 +144,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     # 'http://localhost:5500',
     # 'http://127.0.0.1:5500',
-    # 'http://localhost:3000',
+    'http://localhost:3000',
     # 'http://127.0.0.1:3000',
     # 'http://localhost:8080',
     # 'http://127.0.0.1:8080',
